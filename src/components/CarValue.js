fd5b06d3c82a1c37a400aeae9b1c052ab3931af1
Brad Traversy <antedebelic@yahoo.com>
2023-11-23 12:24:00 +0100

    added functionality to carvalue.js

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,27 @@
+import { useSelector } from "react-redux/";
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    //rješenje s for-loopom:
+
+    /*let cost = 0;
+    for (let car of filteredCars) {
+      cost += car.cost;
+    }
+    return cost;
+*/
+
+    //rješenje korištenjem reduca:
+
+    return filteredCars.reduce((acc, car) => {
+      return acc + car.cost;
+    }, 0);
+  });
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 }
 
 export default CarValue;
